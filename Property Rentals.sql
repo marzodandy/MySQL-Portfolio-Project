16c942728b045616b603fc8fd5f55ab96f0e07c1
Ahmad Marzo Dandy <136870834+marzodandy@users.noreply.github.com>
2024-09-26 13:15:41 +0700

    Update Property Rentals.sql

--- Property Rentals.sql
+++ Property Rentals.sql
@@ -1,6 +1,7 @@
 #AIM
 /*
-To be able to categorize property for rent in San Fransisco based on the property type, minimum nights to book, and the price range
+Categorize property for rental in San Fransisco based on the property type, minimum nights to book, and the price range, 
+providing suggestions for a wide range of guests.
 */
 
 #SUMMARY
@@ -10,7 +11,9 @@ GENERAL
     1. Apartment: 912 units
     2. Houses: 621 units
     3. Condominium: 246 units
-2. Average apartments come with 1 bathroom and 1 bedroom. Houses typically offer 2 bathrooms and 2 bedrooms, while condominiums and townhouses usually feature 1 bathroom and 2 bedrooms.
+2. a) Average apartments come with 1 bathroom and 1 bedroom. 
+   b) Houses typically offer 2 bathrooms and 2 bedrooms
+   c) Condominiums and townhouses usually feature 1 bathroom and 2 bedrooms.
 3. Bed and breakfasts are the most affordable property type, with an average price of $64.1 from 20 units.
 
 SHORT TERM RENTAL
@@ -20,9 +23,19 @@ SHORT TERM RENTAL
 6. Houses remain the most offered property type, with 354 units available for the price range above $100.
 
 LONG TERM RENTAL
-1. There are 846 properties available for monthly or longer-term rentals.
+1. 846 properties are available for longer-term or monthly rentals, dominated by 515 units of apartments.
 2. Hostels are the cheapest long-term rental option, with a minimum stay of 30 days and an average price of $44.50 per night.
 3. A unit bungalow is available for only $40.00 with minimum nights of 30 days.
+*/
+
+#SUGGESTIONS
+/* 
+1. For a family who only plans a short-term rental on a budget, houses are the best option to choose.
+2. For solo and multi-travelers, hostels are the most affordable option to rent for a short period.
+3. If a "user" is looking for the most affordable property in general, bed & breakfasts are available with 20 units.
+4. Based on the amount of units, apartments are the way to go for solo travelers, while 
+   houses, condominiums, and townhouses are for a family or a group of travelers.
+5. Looking for a long-term rental? Apartments are the most offered and are available in both cost-friendly and higher-cost categories.
 */
 
 SELECT * FROM property_rentals.property_rentals; 
@@ -54,7 +67,7 @@ ALTER TABLE property_rentals
 MODIFY COLUMN price INT;
 */
 
--- CHEAP
+-- AFFORDABLE, SHORT-TERM RENTAL PROPERTY PROPERTY
 SELECT *
 FROM property_rentals
 WHERE price <= 100 AND minimum_nights <= 7;
@@ -62,7 +75,7 @@ WHERE price <= 100 AND minimum_nights <= 7;
 SELECT property_type, COUNT(property_type)
 FROM property_rentals
 WHERE price <= 100 AND minimum_nights <= 7
-GROUP BY property_type; -- HOUSE: MOST WIDELY OFFERED PROPERTY TYPE FOR "CHEAP" CATEGORY
+GROUP BY property_type; -- HOUSE: MOST WIDELY OFFERED PROPERTY TYPE FOR THE "AFFORDABLE" CATEGORY
 
 SELECT *
 FROM property_rentals
@@ -77,7 +90,7 @@ GROUP BY property_type; -- HOSTEL: CHEAPEST FOR A WEEK RENT
 SELECT property_type, COUNT(property_type)
 FROM property_rentals
 WHERE price > 100 AND minimum_nights <= 7
-GROUP BY property_type; -- HOUSE: MOST COMMON PROPERTY TYPE OFFERING  LESS THAN OR A WEEK OF RENT
+GROUP BY property_type; -- HOUSE: MOST COMMON PROPERTY TYPE OFFERING LESS THAN OR A WEEK OF RENT
 
 SELECT property_type, AVG(price)
 FROM property_rentals
@@ -124,7 +137,7 @@ WHERE minimum_nights >= 30; -- 846 PROPERTIES AVAILABLE TO BOOK FOR LONGER PERIO
 SELECT property_type, AVG(price)
 FROM property_rentals
 WHERE minimum_nights >= 30
-GROUP BY property_type; -- HOSTEL: CHEAPEST PROPERT TYPE TO RENT FOR 30 DAYS OR MORE
+GROUP BY property_type; -- HOSTEL: CHEAPEST PROPERTY TYPE TO RENT FOR 30 DAYS OR MORE
 
 SELECT property_type, MAX(minimum_nights)
 FROM property_rentals
